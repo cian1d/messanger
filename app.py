9f44b5a501fdf52f3bc911005d3268761039b779
glebe <glebegor1982@gmail.com>
2026-04-03 21:28:02 +0300

    s

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask   import Flask, render_template, request, redirect
+import json
+import os
+
+app = Flask(__name__)
+DATA_FILE = 'messages.json'
+
+
+def load_messages():
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+
+def save_message(text):
+    messages = load_messages()
+    messages.append({"text": text})
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(messages, f, ensure_ascii=False, indent=4)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        text = request.form.get('message')
+        if text:
+            save_message(text)
+        return redirect('/')
+
+    messages = load_messages()
+    return render_template('index.html', messages=messages)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
